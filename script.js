d9510bd99e944703ff709e5fe3f3fb7ae1df18f4
unknown <sarthakjoleya@gmail.com>
2022-12-10 23:15:29 +0530

    calcultor made by chatgpt

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-let display = document.getElementById('result');
+// Get a reference to the display element
+const display = document.getElementById('display');
 
-let buttons = Array.from(document.getElementsByClassName('item'));
+// Function to add input to the display
+function addInput(input) {
+  display.value += input;
+}
 
-buttons.map(item =>{
-    item.addEventListener('click' , (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-            }
-    });
-});
+// Function to clear the input from the display
+function clearInput() {
+  display.value = '';
+}
+
+// Function to calculate the result of the input
+function calculate() {
+  display.value = eval(display.value);
+}
